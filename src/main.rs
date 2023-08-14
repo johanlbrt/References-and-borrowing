065d5ffdd594246ed8ae13714387050a4f98b9c2
Johan Liebert <bggbss@rambler.ru>
2023-08-14 13:44:11 +0300

    function reference

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("Hello!");
+    let len = calc_lenght(&s1);
+    println!("{} = {}",s1,len);
+}
+
+fn calc_lenght(s: &String) -> usize {
+    s.len()
 }
